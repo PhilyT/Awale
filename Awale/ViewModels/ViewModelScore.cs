f28f255f3c8c6d6f9964f0b199663b141da74b47
Tom <tom.phily@live.fr>
2018-04-04 22:54:58 +0200

    Ajout de l'écran de score

--- Awale/ViewModels/ViewModelScore.cs
+++ Awale/ViewModels/ViewModelScore.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Awale.Models;
+using Awale.Utils;
+using Awale.Views;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +14,27 @@ namespace Awale.ViewModels
     public class ViewModelScore : ViewModelBase
     {
         Frame frame;
+        private DelegateCommand retour;
+        private ObservableCollection<Player> joueurs;
         public ViewModelScore(Frame frame)
         {
             this.frame = frame;
+            retour = new DelegateCommand(o => OnClickRetour(o));
+            joueurs = new Sauvegarde().ReadXML();
+            joueurs = new ObservableCollection<Player>(joueurs.OrderBy(joueur => joueur.NbVictoire));
+        }
+        private void OnClickRetour(object o)
+        {
+            MenuView menu = new MenuView(frame);
+            frame.Navigate(menu);
+        }
+        public DelegateCommand Retour { get => retour; set => retour = value; }
+        public ObservableCollection<Player> Joueurs { get => joueurs;
+            set
+            {
+                joueurs = value;
+                RaisePropertyChanged("Joueurs");
+            }
         }
     }
 }
